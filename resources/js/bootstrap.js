b7c20c42e8e69b83a7d493b32b7705683694ab42
mdnurulmomen <mdnurulmomen.bd@gmail.com>
2025-03-24 03:48:23 +0600

    Config for SPA authentication is added

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,6 +10,12 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['Accept'] = 'application/json';
+window.axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+/** SPA Authentication */
+window.axios.defaults.withCredentials = true;
+window.axios.defaults.withXSRFToken = true;
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
