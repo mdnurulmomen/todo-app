41aca942f5c6c4f2db2e834d695aae9e844f4050
mdnurulmomen <mdnurulmomen.bd@gmail.com>
2025-03-24 00:20:36 +0600

    Vue auth-scaffolding is generated

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect('/login');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
