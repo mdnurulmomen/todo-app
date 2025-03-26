3aa0d071cea70b24b1ac5b136033670c9075fa66
mdnurulmomen <mdnurulmomen.bd@gmail.com>
2025-03-27 01:10:44 +0600

    Application front-end is separted

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,9 +9,9 @@ import 'bootstrap';
 import axios from 'axios';
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.headers.common['Accept'] = 'application/json';
-window.axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['Accept'] = 'application/json';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 /** SPA Authentication */
 window.axios.defaults.withCredentials = true;
